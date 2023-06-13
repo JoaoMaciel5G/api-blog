df0efeadf2bfc112e7eb53bdbdb7611236b87eea
JoaoMaciel5G <juauvitu765@gmail.com>
2023-06-13 11:03:30 -0300

    change port

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express()
 const cors = require("cors")
 const routes = require("./routes/routes")
 
+const PORT = process.env.PORT || 8000;
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use("/", routes)
-app.listen(8000)
+app.listen(PORT)
